4496773dee9e85044a347984d654062755d9119b
Keith Howling <khowling@gmail.com>
2016-02-03 12:04:19 +0000

    remove khowling__

--- server/main.js
+++ server/main.js
@@ -33,7 +33,7 @@ function getData(key, canvasobj) {
           'Authorization': 'Bearer ' +canvasobj.client.oauthToken,
         } ,
         query: {
-          q:`select Id, Name, khowling__Leavers__c, khowling__P_L__c from khowling__Bid_P_L__c where khowling__Bid__c = '${canvasobj.context.environment.record.Id}' ORDER BY Id DESC LIMIT 5`
+          q:`select Id, Name, Leavers__c, P_L__c from Bid_P_L__c where Bid__c = '${canvasobj.context.environment.record.Id}' ORDER BY Id DESC LIMIT 5`
         }
       }).on('complete', (sfrecs) => {
         console.log ('finished get version (ms): ' + (Date.now() - st));
@@ -58,7 +58,7 @@ function getData(key, canvasobj) {
           'Authorization': 'Bearer ' +canvasobj.client.oauthToken,
         } ,
         query: {
-          q:`select Id, khowling__Bid__r.Name, khowling__Bid__r.khowling__Contract_Term__c,  khowling__Router_Selection__r.Name, khowling__Router_Selection__r.khowling__Cost__c, khowling__Router_Selection__r.khowling__Markup__c, khowling__Access_Method__r.Name, khowling__Access_Method__r.khowling__One_Off_Cost__c, khowling__Access_Method__r.khowling__Recurring_Cost__c, khowling__Access_Method__r.khowling__Markup__c, khowling__Bandwidth__c, khowling__Bid_Port_Speed__r.khowling__Cost__c, khowling__Bid_Port_Speed__r.khowling__Markup__c from khowling__Bid_Site__c where khowling__Bid__c = '${canvasobj.context.environment.record.Id}'`
+          q:`select Id, Bid__r.Name, Bid__r.Contract_Term__c,  Router_Selection__r.Name, Router_Selection__r.Cost__c, Router_Selection__r.Markup__c, Access_Method__r.Name, Access_Method__r.One_Off_Cost__c, Access_Method__r.Recurring_Cost__c, Access_Method__r.Markup__c, Bandwidth__c, Bid_Port_Speed__r.Cost__c, Bid_Port_Speed__r.Markup__c from Bid_Site__c where Bid__c = '${canvasobj.context.environment.record.Id}'`
         }
       }).on('complete', (sfrecs) => {
         console.log ('finished get bid (ms): ' + (Date.now() - st));
@@ -78,8 +78,8 @@ function getData(key, canvasobj) {
     Promise.all(pWork).then((vals) => {
       let versions =  vals[0], sfrecs = vals[1];
       console.log (`Got all results #sites ${sfrecs && sfrecs.length}, #versions ${versions && versions.length}`);
-      let leavers = versions && JSON.parse(versions[0].khowling__Leavers__c) || {
-          term: sfrecs && parseInt(sfrecs.records[0].khowling__Bid__r.khowling__Contract_Term__c) || 0,
+      let leavers = versions && JSON.parse(versions[0].Leavers__c) || {
+          term: sfrecs && parseInt(sfrecs.records[0].Bid__r.Contract_Term__c) || 0,
           port_discount: 0,
           access_markup: 0,
           cpe_hardware: 0,
@@ -113,17 +113,17 @@ function calcpnl (sfrecs, leavers) {
   };
 
   if (sfrecs && sfrecs.length >0) for (let r of sfrecs) {
-    if (!r.khowling__Router_Selection__r) r.khowling__Router_Selection__r = {khowling__Cost__c: 0, khowling__Markup__c:0};
-    if (!r.khowling__Bid_Port_Speed__r) r.khowling__Bid_Port_Speed__r = {khowling__Cost__c: 0, khowling__Markup__c:0};
-    if (!r.khowling__Access_Method__r) r.khowling__Access_Method__r = {khowling__Recurring_Cost__c: 0, khowling__One_Off_Cost__c:0, khowling__Markup__c:0};
+    if (!r.Router_Selection__r) r.Router_Selection__r = {Cost__c: 0, Markup__c:0};
+    if (!r.Bid_Port_Speed__r) r.Bid_Port_Speed__r = {Cost__c: 0, Markup__c:0};
+    if (!r.Access_Method__r) r.Access_Method__r = {Recurring_Cost__c: 0, One_Off_Cost__c:0, Markup__c:0};
 
-    let router_1cost = r.khowling__Router_Selection__r.khowling__Cost__c,
-        router_price = router_1cost + ((r.khowling__Router_Selection__r.khowling__Markup__c /100) * router_1cost),
-        port_1cost = r.khowling__Bid_Port_Speed__r.khowling__Cost__c,
-        port_price = port_1cost + ((r.khowling__Bid_Port_Speed__r.khowling__Markup__c /100) * port_1cost),
-        access_1cost = r.khowling__Access_Method__r.khowling__One_Off_Cost__c,
-        access_ncost = r.khowling__Access_Method__r.khowling__Recurring_Cost__c * leavers.term,
-        access_price = access_1cost  + ((r.khowling__Access_Method__r.khowling__Markup__c /100) * access_1cost)  + access_ncost  + ((r.khowling__Access_Method__r.khowling__Markup__c / 100) * access_ncost),
+    let router_1cost = r.Router_Selection__r.Cost__c,
+        router_price = router_1cost + ((r.Router_Selection__r.Markup__c /100) * router_1cost),
+        port_1cost = r.Bid_Port_Speed__r.Cost__c,
+        port_price = port_1cost + ((r.Bid_Port_Speed__r.Markup__c /100) * port_1cost),
+        access_1cost = r.Access_Method__r.One_Off_Cost__c,
+        access_ncost = r.Access_Method__r.Recurring_Cost__c * leavers.term,
+        access_price = access_1cost  + ((r.Access_Method__r.Markup__c /100) * access_1cost)  + access_ncost  + ((r.Access_Method__r.Markup__c / 100) * access_ncost),
         revenue = router_price + access_price + port_price;
 
     if (leavers.port_discount >0) revenue -= (port_price / 100.0) * leavers.port_discount;
@@ -162,7 +162,7 @@ app.post('/recalc', jsonParser, function(req, res) {
             let newleavers = Object.assign({}, JSON.parse(data.leavers), req.body.leavers);
             res.json({leavers: newleavers, pnl: calcpnl(JSON.parse(data.sfrecs), newleavers)});
           } else if (req.body.version) {
-            let newleavers = JSON.parse(JSON.parse(data.versions).find((v) => v.Id === req.body.version).khowling__Leavers__c);
+            let newleavers = JSON.parse(JSON.parse(data.versions).find((v) => v.Id === req.body.version).Leavers__c);
             console.log ('newleavers ' + JSON.stringify(newleavers));
             res.json({leavers_current: req.body.version, leavers: newleavers, pnl: calcpnl(JSON.parse(data.sfrecs), newleavers)});
           }
@@ -170,11 +170,11 @@ app.post('/recalc', jsonParser, function(req, res) {
           console.log ('saving');
           let canvasobj = req.session.signedreq,
               pnl = calcpnl(JSON.parse(data.sfrecs), req.body.leavers);
-          rest.postJson ( canvasobj.client.instanceUrl +  canvasobj.context.links.sobjectUrl + 'khowling__Bid_P_L__c',
+          rest.postJson ( canvasobj.client.instanceUrl +  canvasobj.context.links.sobjectUrl + 'Bid_P_L__c',
             {
-              "khowling__Bid__c": canvasobj.context.environment.record.Id,
-              "khowling__Leavers__c": JSON.stringify(req.body.leavers),
-              "khowling__P_L__c": JSON.stringify(pnl)
+              "Bid__c": canvasobj.context.environment.record.Id,
+              "Leavers__c": JSON.stringify(req.body.leavers),
+              "P_L__c": JSON.stringify(pnl)
             },{
             headers: {
               'Authorization': 'Bearer ' +canvasobj.client.oauthToken,
@@ -190,7 +190,7 @@ app.post('/recalc', jsonParser, function(req, res) {
 
 app.get('/go', function(req, res){
   let token = '00D58000000Hlh6!ARMAQJKvnnTwCqkVwY0AcuGAa5.rYViosUCErn2onua5983DAos2pm8IU5PlqfjWOOqJCgtuebJ5_s3Uwz9t5zgV77DMjiGx';
-  let json_envelope = '{"algorithm":"HMACSHA256","issuedAt":1677461276,"userId":"00558000000R5HUAA0","client":{"refreshToken":null,"instanceId":"09H58000000PH5i:","targetOrigin":"https://kforce-dev-ed.my.salesforce.com","instanceUrl":"https://kforce-dev-ed.my.salesforce.com","oauthToken":"'+token+'"},"context":{"user":{"userId":"00558000000R5HUAA0","userName":"khowling@dev.org","firstName":"keith","lastName":"howling","email":"khowling@gmail.com","fullName":"keith howling","locale":"en_GB","language":"en_US","timeZone":"Europe/London","profileId":"00e58000000WBp8","roleId":null,"userType":"STANDARD","currencyISOCode":"GBP","profilePhotoUrl":"https://kforce-dev-ed--c.eu6.content.force.com/profilephoto/005/F","profileThumbnailUrl":"https://kforce-dev-ed--c.eu6.content.force.com/profilephoto/005/T","siteUrl":null,"siteUrlPrefix":null,"networkId":null,"accessibilityModeEnabled":false,"isDefaultNetwork":true},"links":{"loginUrl":"https://kforce-dev-ed.my.salesforce.com","enterpriseUrl":"/services/Soap/c/35.0/00D58000000Hlh6","metadataUrl":"/services/Soap/m/35.0/00D58000000Hlh6","partnerUrl":"/services/Soap/u/35.0/00D58000000Hlh6","restUrl":"/services/data/v35.0/","sobjectUrl":"/services/data/v35.0/sobjects/","searchUrl":"/services/data/v35.0/search/","queryUrl":"/services/data/v35.0/query/","recentItemsUrl":"/services/data/v35.0/recent/","chatterFeedsUrl":"/services/data/v31.0/chatter/feeds","chatterGroupsUrl":"/services/data/v35.0/chatter/groups","chatterUsersUrl":"/services/data/v35.0/chatter/users","chatterFeedItemsUrl":"/services/data/v31.0/chatter/feed-items","userUrl":"/00558000000R5HUAA0"},"application":{"name":"vf-flex","canvasUrl":"https://vf-flex.herokuapp.com","applicationId":"06P58000000PH7e","version":"1.0","authType":"SIGNED_REQUEST","referenceId":"09H58000000PH5i","options":[],"samlInitiationMethod":"None","developerName":"vf_flex","isInstalledPersonalApp":false,"namespace":"khowling"},"organization":{"organizationId":"00D58000000Hlh6EAC","name":"salesforce","multicurrencyEnabled":false,"namespacePrefix":"khowling","currencyIsoCode":"GBP"},"environment":{"locationUrl":"https://kforce-dev-ed.my.salesforce.com/canvas/proxy.jsp?a=%7B%22referenceId%22%3A%2209H58000000PH5i%22%7D&ns=undefined&dn=undefined&ri=09H58000000PH5i&s=00D58000000Hlh6!ARMAQM2DDXYRQ.nZD4TfWLQa6k2cdOOyz1CHuoTcFoPfD60ZFPeAi6LX46gJGRdK.56mBtRUKS.O.VeAJbEG6X4EF_APBrLl&sr=&dm=https://kforce-dev-ed.my.salesforce.com&o=%7B%22frameborder%22%3A%220%22%2C%22width%22%3A%7B%22value%22%3A%22100%25%22%2C%22max%22%3A%221200px%22%7D%2C%22height%22%3A%7B%22value%22%3A%22200px%22%2C%22max%22%3A%22infinite%22%7D%2C%22scrolling%22%3A%22no%22%2C%22displayLocation%22%3A%22PageLayout%22%2C%22showLoadingStatus%22%3Atrue%2C%22record%22%3A%7B%22Id%22%3A%22a05580000004Dwn%22%2C%22fields%22%3A%22%22%7D%2C%22clientWidth%22%3A%221197px%22%2C%22clientHeight%22%3A%2230px%22%7D&v=35.0","displayLocation":"PageLayout","sublocation":null,"uiTheme":"Theme3","dimensions":{"width":"100%","height":"200px","maxWidth":"1200px","maxHeight":"infinite","clientWidth":"1197px","clientHeight":"30px"},"parameters":{},"record":{"attributes":{"type":"khowling__Bid__c","url":"/services/data/v35.0/sobjects/khowling__Bid__c/a05580000004DwnAAE"},"Id":"a05580000004DwnAAE"},"version":{"season":"WINTER","api":"35.0"}}}}';
+  let json_envelope = '{"algorithm":"HMACSHA256","issuedAt":1677461276,"userId":"00558000000R5HUAA0","client":{"refreshToken":null,"instanceId":"09H58000000PH5i:","targetOrigin":"https://kforce-dev-ed.my.salesforce.com","instanceUrl":"https://kforce-dev-ed.my.salesforce.com","oauthToken":"'+token+'"},"context":{"user":{"userId":"00558000000R5HUAA0","userName":"khowling@dev.org","firstName":"keith","lastName":"howling","email":"khowling@gmail.com","fullName":"keith howling","locale":"en_GB","language":"en_US","timeZone":"Europe/London","profileId":"00e58000000WBp8","roleId":null,"userType":"STANDARD","currencyISOCode":"GBP","profilePhotoUrl":"https://kforce-dev-ed--c.eu6.content.force.com/profilephoto/005/F","profileThumbnailUrl":"https://kforce-dev-ed--c.eu6.content.force.com/profilephoto/005/T","siteUrl":null,"siteUrlPrefix":null,"networkId":null,"accessibilityModeEnabled":false,"isDefaultNetwork":true},"links":{"loginUrl":"https://kforce-dev-ed.my.salesforce.com","enterpriseUrl":"/services/Soap/c/35.0/00D58000000Hlh6","metadataUrl":"/services/Soap/m/35.0/00D58000000Hlh6","partnerUrl":"/services/Soap/u/35.0/00D58000000Hlh6","restUrl":"/services/data/v35.0/","sobjectUrl":"/services/data/v35.0/sobjects/","searchUrl":"/services/data/v35.0/search/","queryUrl":"/services/data/v35.0/query/","recentItemsUrl":"/services/data/v35.0/recent/","chatterFeedsUrl":"/services/data/v31.0/chatter/feeds","chatterGroupsUrl":"/services/data/v35.0/chatter/groups","chatterUsersUrl":"/services/data/v35.0/chatter/users","chatterFeedItemsUrl":"/services/data/v31.0/chatter/feed-items","userUrl":"/00558000000R5HUAA0"},"application":{"name":"vf-flex","canvasUrl":"https://vf-flex.herokuapp.com","applicationId":"06P58000000PH7e","version":"1.0","authType":"SIGNED_REQUEST","referenceId":"09H58000000PH5i","options":[],"samlInitiationMethod":"None","developerName":"vf_flex","isInstalledPersonalApp":false,"namespace":"khowling"},"organization":{"organizationId":"00D58000000Hlh6EAC","name":"salesforce","multicurrencyEnabled":false,"namespacePrefix":"khowling","currencyIsoCode":"GBP"},"environment":{"locationUrl":"https://kforce-dev-ed.my.salesforce.com/canvas/proxy.jsp?a=%7B%22referenceId%22%3A%2209H58000000PH5i%22%7D&ns=undefined&dn=undefined&ri=09H58000000PH5i&s=00D58000000Hlh6!ARMAQM2DDXYRQ.nZD4TfWLQa6k2cdOOyz1CHuoTcFoPfD60ZFPeAi6LX46gJGRdK.56mBtRUKS.O.VeAJbEG6X4EF_APBrLl&sr=&dm=https://kforce-dev-ed.my.salesforce.com&o=%7B%22frameborder%22%3A%220%22%2C%22width%22%3A%7B%22value%22%3A%22100%25%22%2C%22max%22%3A%221200px%22%7D%2C%22height%22%3A%7B%22value%22%3A%22200px%22%2C%22max%22%3A%22infinite%22%7D%2C%22scrolling%22%3A%22no%22%2C%22displayLocation%22%3A%22PageLayout%22%2C%22showLoadingStatus%22%3Atrue%2C%22record%22%3A%7B%22Id%22%3A%22a05580000004Dwn%22%2C%22fields%22%3A%22%22%7D%2C%22clientWidth%22%3A%221197px%22%2C%22clientHeight%22%3A%2230px%22%7D&v=35.0","displayLocation":"PageLayout","sublocation":null,"uiTheme":"Theme3","dimensions":{"width":"100%","height":"200px","maxWidth":"1200px","maxHeight":"infinite","clientWidth":"1197px","clientHeight":"30px"},"parameters":{},"record":{"attributes":{"type":"Bid__c","url":"/services/data/v35.0/sobjects/Bid__c/a05580000004DwnAAE"},"Id":"a05580000004DwnAAE"},"version":{"season":"WINTER","api":"35.0"}}}}';
   let canvasRequest = JSON.parse(json_envelope);
 
   req.session.signedreq = canvasRequest;
